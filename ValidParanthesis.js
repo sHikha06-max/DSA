3de76d152135a71461a3ec9e476e1500e26d854f
shikha <mshikha38@gmail.com>
2023-02-14 20:07:38 +0800

    valid paranthesis

---
+++ ValidParanthesis.js
@@ -0,0 +1,36 @@
+var isValid = function(s) {
+    var S = s.split("");
+         var stack=[];
+     var check=0;
+     for(var j = 0;j<S.length;j++){
+          if(S[j]=='{'|| S[j]=='('|| S[j]=='['){
+       
+       stack.push(s[j]);
+       }
+       else{
+       
+           if(S[j]=='}' && stack[stack.length-1]=='{' || S[j]==']' && stack[stack.length-1]=='[' || s[j]==')' && stack[stack.length-1]=='('){
+           stack.pop(); 
+           }
+     
+         else{
+          check=1;
+          break;
+         }
+     
+       }
+  
+     }
+    if(check==1){
+   return false;   
+      
+    }
+    
+    else if( stack.length==0){
+   return true;
+    }
+    
+    else{
+    return false;
+    }  
+};
