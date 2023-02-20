20bd791d8ee85ea496830865b579048bf700c205
shikha <mshikha38@gmail.com>
2023-02-20 11:29:09 +0800

    done

---
+++ Arrays/MaxConsecutiveOnes.js
@@ -0,0 +1,16 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+        let max = 0;
+        for(let i = 0; i < nums.length; i++) {
+          if(nums[i] == 1) {
+            count ++;
+             max = Math.max(max, count);
+          } else {
+            count = 0;
+          }
+        }
+        return max;
+      
+    };
+
+    //Time complexity O(N)
