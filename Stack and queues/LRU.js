4d859474abf53295f9b7ebd2ba7242a420f05ef2
shikha <mshikha38@gmail.com>
2023-02-22 15:18:57 +0800

    LRU

---
+++ Stack and queues/LRU.js
@@ -0,0 +1,37 @@
+var LRUCache = function(capacity) {
+    this.capacity = capacity;
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if(!this.cache.has(key)){
+        return -1
+    }
+    
+    let val = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, val)
+    return val
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if(this.cache.has(key)){
+        this.cache.delete(key);
+    }else{
+        if(this.cache.size >= this.capacity){
+            let [lastElm] = this.cache.keys()
+            this.cache.delete(lastElm)
+        }
+    }
+    this.cache.set(key, value)
+    
+};
