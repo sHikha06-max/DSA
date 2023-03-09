84136854675d580f8d319cbefcb7622ec0eae2b7
shikha <mshikha38@gmail.com>
2023-03-09 12:01:18 +0800

    Not boring movies

---
+++ SQL/NotBroingMovie.sql
@@ -0,0 +1,8 @@
+/*
+Write an SQL query to report the movies with an odd-numbered ID and a description that is not "boring".
+*/
+
+select id, movie, description, rating
+from Cinema
+where (id % 2) = 1 and description != "boring"
+order by rating desc
