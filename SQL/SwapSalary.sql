71666680cdee6a9d84ace4dbd8cd9571aa41780e
shikha <mshikha38@gmail.com>
2023-03-09 11:45:51 +0800

    swap salary

---
+++ SQL/SwapSalary.sql
@@ -0,0 +1,11 @@
+/*
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all
+ 'f' values to 'm' and vice versa) with a single update statement and no intermediate temporary tables.
+*/
+
+UPDATE salary
+SET sex = CASE
+ WHEN sex = 'm' 
+ THEN 'f'
+  ELSE 'm'
+   END
